29292af0277f901d7a0ab786b51138d0b1191212
Edward <edpantzar@gmail.com>
2018-06-10 10:28:57 -0500

    Added scroll spy and some other small changes

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
+  
+  let navElems = document.querySelectorAll('.sidenav');
+  let navInstances = M.Sidenav.init(navElems, {draggable: true});
+
+  let spyElems = document.querySelectorAll('.scrollspy');
+  let spyInstances = M.ScrollSpy.init(spyElems, {scrollOffset: 50});
+
 });
